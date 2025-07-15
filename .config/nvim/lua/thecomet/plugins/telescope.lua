11f98fd0b6118224df224b57ba19c15791135ffa
Alexander Murray <alexander.murray@franke.com>
2025-07-15 12:12:48 +0200

    Ignore telescope

--- .config/nvim/lua/thecomet/plugins/telescope.lua
+++ .config/nvim/lua/thecomet/plugins/telescope.lua
@@ -44,29 +44,27 @@ local live_multigrep = function(opts)
 end
 
 return {
-    "nvim-telescope/telescope.nvim",
-    tag = "0.1.8",
-    dependencies = {
-        "nvim-lua/plenary.nvim"
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.8",
+  dependencies = {
+    "nvim-lua/plenary.nvim"
+  },
+  opts = {
+    defaults = {
+      file_ignore_patterns = {
+        "^ShadowOrbot/",
+        "^ThirdParty/",
+      }
     },
-    opts = {
-        file_ignore_patterns = {
-            "build/",
-            "build_cppcheck/",
-        },
-        pickers = {
-            colorscheme = { enable_preview = true }
-        },
-    },
-    config = function(_, opts)
-        require("telescope").setup(opts)
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', "<leader>pf", builtin.find_files, {})
-        vim.keymap.set('n', "<leader>pg", builtin.git_files, {})
-        --vim.keymap.set('n', "<leader>ps", builtin.live_grep, {})
-        vim.keymap.set('n', "<leader>ps", live_multigrep, {})
-        vim.keymap.set('n', "gs", builtin.lsp_document_symbols, {})
-        vim.keymap.set('n', "gc", builtin.lsp_incoming_calls, {})
-    end
+  },
+  config = function(_, opts)
+    require("telescope").setup(opts)
+    local builtin = require('telescope.builtin')
+    vim.keymap.set('n', "<leader>pf", builtin.find_files, {})
+    vim.keymap.set('n', "<leader>pg", builtin.git_files, {})
+    --vim.keymap.set('n', "<leader>ps", builtin.live_grep, {})
+    vim.keymap.set('n', "<leader>ps", live_multigrep, {})
+    vim.keymap.set('n', "gs", builtin.lsp_document_symbols, {})
+    vim.keymap.set('n', "gc", builtin.lsp_incoming_calls, {})
+  end
 }
-
